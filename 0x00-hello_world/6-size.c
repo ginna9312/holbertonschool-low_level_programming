17309116eefa38836e42e398516a522abacfa02e
Ginna <g.i.maro@hotmail.com>
2021-02-04 03:51:30 +0000

    write

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,9 +8,9 @@
  */
 int main(void)
 {
-	printf(strcpy("Size of a char: ",sizeof(char),"byte(s)\n")x)
+	printf(strcpy("Size of a char: ", sizeof(char), "byte(s)\n")x)
 	printf("Size of an int: 4 byte(s)\n");
-	printf(strcpy("Size of a long int: ",sizeof(long int),"byte(s)\n");)
+	printf(strcpy("Size of a long int: ", sizeof(long int), "byte(s)\n");)
 	printf("Size of a long long int: 8 byte(s)\n");
 	printf("Size of a float: 4 byte(s)\n");
 	return (0);
