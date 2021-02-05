802186f85f038b9be2ee92d98eec3b7b878a631f
Ginna <g.i.maro@hotmail.com>
2021-02-05 01:55:47 +0000

    read

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <string.h>
-
 /**
  * main - Entry point
  *
@@ -8,7 +7,6 @@
  */
 int main(void)
 {
-
-	puts(" Programming is like building a multilingual puzzle");
+	puts("\" Programming is like building a multilingual puzzle");
 return (0);
 }
