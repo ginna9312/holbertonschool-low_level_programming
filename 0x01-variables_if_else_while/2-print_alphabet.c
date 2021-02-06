4427c0a68d9b551c5369dd7d3ddfe974b766e123
Ginna <g.i.maro@hotmail.com>
2021-02-06 00:11:02 +0000

    read

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/* more headers goes there */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	char a;
+	for (a = 'a'; a <= 'z'; a++)
+		{
+			putchar(a);
+		}
+	putchar ('\n');
+		return (0);
+}
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	num_ramdon = (rand & n+1)
+		return (0);
+}
