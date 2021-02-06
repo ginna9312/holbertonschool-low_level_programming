417222edbd4bf39706ecf809bae48944a7b3360e
Ginna <g.i.maro@hotmail.com>
2021-02-06 01:42:58 +0000

    read

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,13 +1,24 @@
-#include <stdlib.h>
-#include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - putchar for
+ * print alphabet a-z A-Z
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	int n;
+	char low, upp;
 
-	srand(time(0));
-	num_ramdon = (rand & n+1)
-		return (0);
+	for (low = 'a'; low <= 'z'; low++)
+	{
+		putchar(low);
+	}
+
+	for (upp = 'A'; upp <= 'Z'; upp++)
+	{
+		putchar(upp);
+	}
+	putchar('\n');
+	return (0);
+
 }
