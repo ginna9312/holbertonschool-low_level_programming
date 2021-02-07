eac5ff831f1feecf05806d58d1209734ac2c5068
Ginna <g.i.maro@hotmail.com>
2021-02-07 16:43:18 +0000

    read

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,15 +12,15 @@ int main(void)
 d = n % 18;
 	if (d > 5)
 	{
-		printf("las digit of %d is %d and is grater than 5\n", n, d);
+		printf("las digit is %d and is grater than 5\n", n, d);
 	}
 	else if (d == 0)
 	{
-		printf("las digit of %d is %d and is 0\n", n, d);
+		printf("las digit is %d and is 0\n", n, d);
 	}
 	else
 	{
-		printf("last digit of %d is %d and is less than 6 and not 0\n", n, d);
+		printf("last digit is %d and is less than 6 and not 0\n", n, d);
 	}
 	return (0);
 }
