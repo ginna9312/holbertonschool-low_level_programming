e50d98aee196058bea2dc5e0c9d96af1f95f3ea0
Ginna <g.i.maro@hotmail.com>
2021-02-09 17:43:17 +0000

    read

---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,48 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+/*prints a char to the stdout*/
+int _putchar(char c);
+
+/*prints a char to the stdout*/
+	void print_alphabet(void);
+
+/*prints a char to the stdout*/
+void print_alphabet_x10(void);
+
+/*prints a char to the stdout*/
+int _islower(int c);
+
+/*prints a char to the stdout*/
+int _isalpha(int c);
+
+/*prints a char to the stdout*/
+int print_sign(int n);
+
+/*prints a char to the stdout*/
+int _abs(int);
+
+/*prints a char to the stdout*/
+int print_last_digit(int);
+
+/*prints a char to the stdout*/
+void jack_bauer(void);
+
+/*prints a char to the stdout*/
+void times_table(void);
+
+/*prints a char to the stdout*/
+int add(int, int);
+
+/*prints a char to the stdout*/
+void print_to_98(int n);
+
+#endif
