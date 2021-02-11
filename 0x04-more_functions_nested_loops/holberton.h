2e79d262b316c6feac3114ca143d679fbccea730
Ginna <g.i.maro@hotmail.com>
2021-02-11 18:15:54 +0000

    read

---
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -0,0 +1,43 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+/*prints a char to the stdout*/
+int _isupper(int c);
+
+/*prints a char to the stdout*/
+int _isdigit(int c);
+
+/*prints a char to the stdout*/
+int mul(int a, int b);
+
+/*prints a char to the stdout*/
+void print_numbers(void);
+
+/*prints a char to the stdout*/
+void print_most_numbers(void);
+
+/*prints a char to the stdout*/
+void more_numbers(void);
+
+/*prints a char to the stdout*/
+void print_line(int n);
+
+/*prints a char to the stdout*/
+void print_diagonal(int n);
+
+/*prints a char to the stdout*/
+void print_square(int size);
+
+/*prints a char to the stdout*/
+void print_triangle(int size);
+
+/*prints a char to the stdout*/
+
+#endif
