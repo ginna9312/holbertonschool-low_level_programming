a626023d796af006d5434f462f262cc7a08b4bf2
Ginna <g.i.maro@hotmail.com>
2021-02-11 23:39:45 +0000

    file

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * print_numbers - from 1 to 9
+ */
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		_putchar(i + '0');
+		++i;
+	}
+	_putchar ('\n');
+}
