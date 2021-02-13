bc885190319759db30ff47b61d32bfaff5675c85
Ginna <g.i.maro@hotmail.com>
2021-02-13 04:21:53 +0000

    change

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ * main - putchar for
+ *  print whether the number stored in the variable n is positive or negative.
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
@@ -25,4 +27,3 @@ int main(void)
 
 	return (0);
 }
-
--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,9 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ * main - putchar if
+ * print the last digit of the number stored in the variable n
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n, d;
