e140b8cd77813ae887d5ec33e74f841adb10950e
Ginna <g.i.maro@hotmail.com>
2021-02-16 18:38:15 +0000

    file

---
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -0,0 +1,49 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+/*prints a char to the stdout*/
+void reset_to_98(int *n);
+
+/*prints a char to the stdout*/
+void swap_int(int *a, int *b);
+
+/*prints a char to the stdout*/
+int _strlen(char *s);
+
+/*prints a char to the stdout*/
+int _putchar(char c);
+
+/*prints a char to the stdout*/
+void _puts(char *str);
+
+/*prints a char to the stdout*/
+void print_rev(char *s);
+
+/*prints a char to the stdout*/
+void rev_string(char *s);
+
+/*prints a char to the stdout*/
+void puts2(char *str);
+
+/*prints a char to the stdout*/
+void puts_half(char *str);
+
+/*prints a char to the stdout*/
+void print_array(int *a, int n);
+
+/*prints a char to the stdout*/
+char *_strcpy(char *dest, char *src);
+
+
+
+
+
+
+
+#endif
