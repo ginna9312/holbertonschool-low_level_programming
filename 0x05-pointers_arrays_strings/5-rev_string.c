f1657365e8e8d8eed2395f2a9f4f89ebae42e89e
Ginna <g.i.maro@hotmail.com>
2021-02-18 04:43:12 +0000

    read

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+# include "holberton.h"
+/**
+ *rev_string - reverses a string
+ *@s:string
+ */
+void rev_string(char *s)
+{
+	int a, j;
+	char r[10];
+
+a = j = 0;
+	while (s[a] != '\0')
+	{
+		r[a] = s[a];
+		a++;
+	}
+	a--;
+	while (a >= 0)
+	{
+		s[a] = r[j];
+		a--;
+		j++;
+	}
+	s[j++] = '\0';
+
+}
