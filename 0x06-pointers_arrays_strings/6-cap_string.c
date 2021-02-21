4904eed2a186cb93a6b5f20f9d22fbc8c499995b
Ginna <g.i.maro@hotmail.com>
2021-02-21 22:33:08 +0000

    read

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+/**
+ * cap_string - makes first letter of each word capital.
+ * Description: see above
+ * @p: array to change
+ * Return: returns the array
+ */
+char *cap_string(char *p)
+{
+	int count;
+
+	count = 0;
+
+	if (p[count] >= 'a' && p[count] <= 'z')
+	{
+		p[count] = p[count] - 32;
+		count++;
+	}
+	while (p[count] != '\0')
+	{
+		if (p[count] == ' ' || p[count] == ',' || p[count] == ';'
+		    || p[count] == '.' || p[count] == '!' || p[count] == '?'
+		    || p[count] == '"' || p[count] == '(' || p[count] == ')'
+		    || p[count] == '(' || p[count] == ')' || p[count] == '{'
+		    || p[count] == '}' || p[count] == '\t' || p[count] == '\n')
+		{
+			if (p[count + 1] >= 'a' && p[count + 1] <= 'z')
+			{
+				p[count + 1] = p[count + 1] - 32;
+				count++;
+			}
+			else
+			{
+				count++;
+			}
+
+		}
+	}
+
+	return (p);
+}
